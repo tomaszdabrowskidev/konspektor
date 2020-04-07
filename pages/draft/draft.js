02aee56d9ccc9577358b3049507b181c27e07d43
mobaradev <user@PC.localdomain>
2020-04-07 23:22:24 +0200

    2322

--- pages/draft/draft.js
+++ pages/draft/draft.js
@@ -4,28 +4,34 @@ window.addEventListener("DOMContentLoaded", () => {
     for(let i = 0; i < 7; i++) {
         document.getElementById('sidebar__element-' + i).addEventListener("click", () => {
             console.log('klikam -> ' + i);
-            Drafts.changePhase = i
+            Drafts.changePhase = i;
         })
     }
     document.getElementById("draft__phase-0-textarea").addEventListener("input", () => {
         if (document.getElementById("draft__phase-0-textarea").value == "szczepan szczesiak") {
             window.location.href = "../about/about.html"
         };
-    })
+    });
+    document.getElementById('draft__button-next').addEventListener('click', () => Drafts.changePhase = "+")
+    document.getElementById('draft__button-previous').addEventListener('click', () => Drafts.changePhase = "-")
 });
 
 class Drafts { // i nie dzialalo? no xd
     static currentPhase = 0;
     
     static set changePhase(phase) {
+        if(phase == "+") phase = this.currentPhase + 1;
+        if(phase == "-") phase = this.currentPhase - 1;
         console.log('Set ' + phase + ' from old ' + this.currentPhase);
+        document.getElementById(`draft__phase-${phase}`).scrollTo(0,0);
         //document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('draft__phase--up')
-        setTimeout(() => {
-            document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('draft__phase--up')
-            this.currentPhase = phase;
-            document.getElementById(`draft__phase-${phase}`).classList.remove('draft__phase--down');
-        }, 3000)
+        document.getElementById(`sidebar__element-${this.currentPhase}`).classList.remove('sidebar__element--selected');
 
+        //document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('draft__phase--up')
+        this.currentPhase = phase;
+        //document.getElementById(`draft__phase-${phase}`).classList.remove('draft__phase--down');
+        document.getElementById('draft__phases').style.transform = `translateY(${(-100) * phase}%)`;
+        document.getElementById(`sidebar__element-${this.currentPhase}`).classList.add('sidebar__element--selected');
     }
 }
 
---
+++ pages/main.js
@@ -0,0 +1,17 @@
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    // enable TAB on Textareas
+    let textareas = document.getElementsByTagName('textarea');
+    for(var i=0;i<textareas.length;i++){
+        textareas[i].onkeydown = function(e){
+            if(e.keyCode==9 || e.which==9){
+                e.preventDefault();
+                var s = this.selectionStart;
+                this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+                this.selectionEnd = s+1; 
+            }
+        }
+    }
+});
