bc954b3913f61ab2feb7d6ee7cae798a2e65c0dc
ttomek <tomek@9lossie.pl>
2020-04-07 02:59:57 +0200

    kajetanChanges

--- pages/draft/draft.js
+++ pages/draft/draft.js
@@ -4,25 +4,28 @@ window.addEventListener("DOMContentLoaded", () => {
     for(let i = 0; i < 7; i++) {
         document.getElementById('sidebar__element-' + i).addEventListener("click", () => {
             console.log('klikam -> ' + i);
-            Drafts.changePhase(i)
+            Drafts.changePhase = i
         })
     }
-
+    document.getElementById("draft__phase-0-textarea").addEventListener("input", () => {
+        if (document.getElementById("draft__phase-0-textarea").value == "szczepan szczesiak") {
+            window.location.href = "../about/about.html"
+        };
+    })
 });
-//siema kajetan
-//normalnie winrar.open
 
-class Drafts {
-    static constructor() {
-        this.currentPhase = 0;
-    }
+class Drafts { // i nie dzialalo? no xd
+    static currentPhase = 0;
     
     static set changePhase(phase) {
         console.log('Set ' + phase + ' from old ' + this.currentPhase);
+        //document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('draft__phase--up')
+        setTimeout(() => {
+            document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('draft__phase--up')
+            this.currentPhase = phase;
+            document.getElementById(`draft__phase-${phase}`).classList.remove('draft__phase--down');
+        }, 3000)
 
-        document.getElementById(`draft__phase-${this.currentPhase}`).classList.add('hidden');
-        this.currentPhase = phase;
-        document.getElementById(`draft__phase-${phase}`).classList.remove('hidden');
     }
 }
 
@@ -31,10 +34,4 @@ class Drafts {
 /*
 🎤🐢Midunseti nane, nane nane, sarebazi bade, bade bade, morakhazish kame, kame kame, naneke-uo mano zayidi, tuo ke dahan-e mano gayidi, midunesti nane, buozorg misham sarebazi miram, chera dadi duoshidam, nazashti man bemiram, sarbazamuo, sarebazi baram ye muoshdekhake, hamin daru nadaram, faghad yik-dune pake, sarbaz duoäm gelim-e pare, shabra sar miyare, kalogh par, pa morghe, basash farghi nadare!🎤🐢
 XDDD
-
-remix
-
-https://www.youtube.com/watch?v=utcID57IbPc
-
-d-.-b
 */
